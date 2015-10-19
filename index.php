a7584978ae57d13f809c841477cbb39ea1428be7
ssuleymanaylak <ssuleymanaylak@hotmail.com>
2015-10-19 09:02:45 +0300

    Login system

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'core/init.php';
+if (Session::exists('home')) {
+	echo '<p>' . Session::flash('home') . '</p>';
+}
+
+$user = new User(); //current user
+
+if ($user->isLoggedIn()) {
+	?>
+	<p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username);?>"><?php echo escape($user->data()->username);?></a> !</p>
+	<ul>
+		<li><a href="logout.php">Log out</a></li>
+		<li><a href="update.php">Update Details</a></li>
+		<li><a href="changepassword.php">Change Password</a></li>
+	</ul>
+
+<?php
+
+	if ($user->hasPermission('admin')) {
+		echo '<p>You are an administrator!</p>';
+	}
+} else {
+	echo '<p>You need to <a href="login.php">log in </a>or <a href="register.php">register</a></p>';
+}
